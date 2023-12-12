2d64a417a70eb80c7531d77f116518cfb0af166f
wooyakob <jake_wood@mac.com>
2023-12-12 15:19:47 -0800

    hungry method added

--- ri20min.rb
+++ ri20min.rb
@@ -70,3 +70,16 @@ else
   puts "#{a} is not equal to 100"
 end
 
+
+def hungry?(time_of_day_in_hours)
+  puts "Me hungry"
+  true
+end
+
+def eat_an(what)
+  puts "Me eat #{what}\n"
+end
+
+eat_an 'apple' if hungry?(14)
+
+eat_an 'apple' if hungry?(10)
